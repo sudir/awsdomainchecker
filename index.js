d32f57d526496174f7316aae29d0b9fd4f052023
root <root@ip-172-31-2-125.ec2.internal>
2017-06-27 04:44:41 +0000

    new project

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const bodyparser = require('body-parser');
+
+// AWS Required 
+const AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+AWS.config.apiVersions = {
+  route53domains: '2014-05-15',
+};
+
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
+
+
+app.get('/', function(req, res){
+    console.log('GET /')
+
+  const html = fs.readFileSync('index.html');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+});
+
+app.post('/', function(req, res){
+    console.log('POST /');
+    console.dir(req.body);
+const route53domains = new AWS.Route53Domains();
+res.writeHead(200, {'Content-Type': 'text/html'});
+
+
+
+const params = req.body;
+route53domains.checkDomainAvailability(params, function(err, data) {
+if (err) console.log(err, err.stack); // an error occurred
+else  res.end('This Domain is: ' + data.Availability);
+})
+ 
+});
+       
+
+port = 8135;
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
+
